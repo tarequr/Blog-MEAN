54fc1bd99d1c3cd4b0b6832c4b6606b0275ca222
tarequr <tarequrr8@gmail.com>
2024-08-28 22:31:31 +0600

    core: date

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios';
 import './App.css'
 
 function App() {
+  const [ postData, setPostData ] = useState();
+
+  useEffect(() => {
+    async function grabData() {
+      const response = await axios.get("http://localhost:3000/api/v1/posts");
+      // console.log(response);
+      if (response.status === 200) {
+        setPostData(response.data);
+      }
+    }
+
+    grabData();
+  }, []);
+
   return (
     <>
-      
+      { JSON.stringify(postData) }
     </>
   )
 }
