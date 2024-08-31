9a7e665c52a3a00fd66a03e28a32bed50a8810bd
tarequr <tarequrr8@gmail.com>
2024-08-31 19:42:48 +0600

    Initialize blog site structure with key JSX components and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,19 @@
-import { useState, useEffect } from 'react'
-import { getPosts, getPost, createPost, updatePost, deletePost } from './api';
-
 import './App.css'
 
 function App() {
-  const [ postData, setPostData ] = useState();
+  //Pages
 
-  async function createPostHandler() {
-    let postObject = {
-      title: "This is a system title 2",
-      description: "This is a system description 2",
-      content: "This is a system content 2",
-      author: "Md. System Author 2",
-    }
-
-    createPost(postObject);
-  }
-
-  useEffect(() => {
-    async function loadPosts() {
-      let data = await getPosts();
-      if (data) {
-        setPostData(data);
-      }
-    }
-
-    loadPosts();
-  }, []);
-
+  //Landing pages
+  //Home page (Fitered by recency)
+  //ReadBlog
+  //CreateBlog
+  //Profile
+  //About
+  //Contact
+  
   return (
     <>
-      { JSON.stringify(postData) }
-      <br />
-      <br />
-
-      <button onClick={ createPostHandler }>
-        Create Post
-      </button>
+      
     </>
   )
 }
