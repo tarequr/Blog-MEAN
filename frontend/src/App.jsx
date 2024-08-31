d79864a98035d85b405e252559c5ee7962c06aa5
tarequr <tarequrr8@gmail.com>
2024-08-31 20:19:26 +0600

    Install react-router-dom and set up basic routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,13 @@
 import './App.css'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { About } from './pages/About';
+import { Contact } from './pages/Contact';
+import { CreateBlog } from './pages/CreateBlog';
+import { Home } from './pages/Home';
+import { Landing } from './pages/Landing';
+import { Profile } from './pages/Profile';
+import { ReadBlog } from './pages/ReadBlog';
 
 function App() {
   //Pages
@@ -12,10 +21,18 @@ function App() {
   //Contact
   
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+        <Routes>
+            <Route path="/landing" element={<Landing />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/createblog" element={<CreateBlog />} />
+            <Route path="/readblog" element={<ReadBlog />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+        </Routes>
+    </Router>
+  );
 }
 
 export default App
