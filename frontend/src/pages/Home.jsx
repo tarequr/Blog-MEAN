3753d7ca0e14f6fc5338ea66ffbef78e8debe1e4
tarequr <tarequrr8@gmail.com>
2024-09-01 23:01:38 +0600

    core: display all posts

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { getPosts } from '../api'
+import { useState, useEffect } from 'react'
 
 function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function getPostsHandel() {
+      const data = await getPosts();
+      // console.log(data);
+      setPosts(data.posts);
+    }
+
+    getPostsHandel();
+  }, []);
+
   return (
-    <div>Home</div>
+    <>
+      { posts.map((post) => {
+        return(
+          <>
+            <h1>{ post.title }</h1>
+            <h2>{ post.description }</h2>
+            <p>{ post.createdAt }</p>
+          </>
+        )
+      }) }
+    </>
   )
 }
 
