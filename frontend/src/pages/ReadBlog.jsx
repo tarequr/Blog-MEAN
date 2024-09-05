74f5d03411c1d3bce898ec237f76bdb5a8345b52
tarequr <tarequrr8@gmail.com>
2024-09-05 22:19:35 +0600

    Core: desplay single post

--- frontend/src/pages/ReadBlog.jsx
+++ frontend/src/pages/ReadBlog.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import { getPost } from '../api'
+// import {useParams} from 'react-router-form'
+import { useNavigate, useParams } from "react-router-dom";
 
 function ReadBlog() {
+  const [post, setPost] = useState({});
+
+  const navigate = useNavigate();
+  let params = useParams();
+  let id = params.id
+  
+  useEffect(() => {
+    async function fetchPostData() {
+      let allData = await getPost(id);
+      let date = new Date(allData.post.createdAt);
+
+      allData.post.createdAt = date.toString();
+      setPost(allData.post);
+    }
+
+    fetchPostData();
+  }, []);
+
   return (
-    <div>ReadBlog</div>
+    <>
+      <button onClick={() => navigate(-1)}>Back</button>
+      <h1>{ post.title }</h1>
+      <h2>{ post.description }</h2>
+      <h3>{ post.createdAt?.slice(4 ,15) }</h3>
+      <p>{ post.content }</p>
+    </>
   )
 }
 
