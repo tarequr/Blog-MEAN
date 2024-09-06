052ab78dcbbf6475b04a4c6d4610204b2f23c7ee
tarequr <tarequrr8@gmail.com>
2024-09-06 20:14:56 +0600

    Core: create blog form

--- frontend/src/pages/CreateBlog.jsx
+++ frontend/src/pages/CreateBlog.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import { useState } from 'react'
+import { createPost } from '../api';
 
 function CreateBlog() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [content, setContent] = useState("");
+
+  async function handleSubmit() {
+    let submitObject = {
+      title: title,
+      description: description,
+      content: content,
+      author: null
+    }
+
+    await createPost(submitObject);
+  }
+
   return (
-    <div>CreateBlog</div>
+    <>
+      <form onSubmit={handleSubmit}>
+
+        <label htmlFor="">Title</label>
+        <input type="text" onChange={(event) => setTitle(event.target.value)} name='title' placeholder='Enter title' />
+
+        <label htmlFor="">Description</label>
+        <input type="text" onChange={(event) => setDescription(event.target.value)} name='description' placeholder='Enter description' />
+
+        <label htmlFor="">Content</label>
+        <textarea name="content" id="" onChange={(event) => setContent(event.target.value)} placeholder='Type here...'></textarea>
+
+        <button type="submit">Submit</button>
+
+      </form>
+    </>
   )
 }
 
