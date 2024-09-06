31bd68fb2c566004bb3ad0bb10c29435905d31f7
tarequr <tarequrr8@gmail.com>
2024-09-06 21:30:53 +0600

    Add Contact page with form for user inquiries and email link

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 
 function Contact() {
+  async function handleSubmit() {
+    // Create  
+  }
+
   return (
-    <div>Contact</div>
+    <div>
+      <h1>Contact Us</h1>
+      <p>Have questions or feedback? Feel free to reach out to us!</p>
+      <p>You can contact us via email at <a href="mailto:contact@example.com">contact@example.com</a> .</p>
+      <p>Alternatively, you can fill out the form below:</p>
+    
+      <form onSubmit={handleSubmit}>
+
+        <label htmlFor="">Name:</label>
+        <input type="text" name='name' />
+
+        <label htmlFor="">Email:</label>
+        <input type="email" name='email' />
+
+        <label htmlFor="">Message:</label>
+        <textarea name="message" id=""></textarea>
+
+        <button type='submit'>Send</button>
+      </form>
+    </div>
   )
 }
 
