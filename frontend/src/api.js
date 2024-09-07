8546bcbed2db1ae8c47532586d48ac31d55cc298
tarequr <tarequrr8@gmail.com>
2024-09-07 16:36:03 +0600

    feat: add user API functions for CRUD operations for frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -41,3 +41,48 @@ export async function deletePost(id) {
     const response = await axios.delete(`${URL}/posts/${id}`);
     return response
 }
+
+
+
+
+/******************************** USER API ********************************/
+
+export async function getUsers() {
+    // http://localhost:3000/api/v1/users
+    const response = await axios.get(`${URL}/users`);
+
+    if (response.status === 200) {
+        return response.data;
+    }
+
+    return
+}
+
+export async function getUser(id) {
+    // http://localhost:3000/api/v1/users/12345
+    const response = await axios.get(`${URL}/users/${id}`);
+
+    if (response.status === 200) {
+        return response.data;
+    }
+
+    return
+}
+
+export async function createUser(user) {
+    // http://localhost:3000/api/v1/users
+    const response = await axios.post(`${URL}/users/create`, user);
+    return response
+}
+
+export async function updateUser(id, post) {
+    // http://localhost:3000/api/v1/users
+    const response = await axios.post(`${URL}/users/${id}`, post);
+    return response
+}
+
+export async function deleteUser(id) {
+    // http://localhost:3000/api/v1/users/12345
+    const response = await axios.delete(`${URL}/users/${id}`);
+    return response
+}
