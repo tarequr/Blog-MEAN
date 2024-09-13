80b4f439f9b4f486922eb25d046426438413969d
tarequr <tarequrr8@gmail.com>
2024-09-13 23:11:23 +0600

    Core: user logout

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,22 +1,33 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 import { pageData } from './pageData'
 
 function Navbar() {
-  return (
-    <div className='navbar'>
-        { pageData.map((page, index) => {
-            return (
-                <Link to={ page.path } key={index} className='navItem'>
-                    <button>
-                        { page.name }
-                    </button>
-                </Link>
-            )
-        }) }
-    </div>
-  )
+
+    const navigate = useNavigate();
+
+    function handleLogout() {
+        sessionStorage.removeItem("User");
+        navigate("/");
+    }
+
+    return (
+        <div className='navbar'>
+            {pageData.map((page, index) => {
+                return (
+                    <Link to={page.path} key={index} className='navItem'>
+                        <button>
+                            {page.name}
+                        </button>
+                    </Link>
+                )
+            })}
+
+            <button onClick={handleLogout}>Log Out</button>
+        </div>
+    )
 }
 
 export default Navbar
